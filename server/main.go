ca05792f1bffded3a7823a1de779d88ac9fb77cb
ilya-tymoshenko <illia.tymoshenko77@gmail.com>
2025-12-09 21:55:13 +0100

    docker logic

--- server/main.go
+++ server/main.go
@@ -1,18 +1,35 @@
 package main
 
 import (
+	"math/rand"
+	"net/http"
+	"time"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
+const charset = "abcdefghijkmnpqrstuvwxyz23456789"
+
+func generateRoomID(length int) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
 	hub := NewHub()
 	go hub.Run()
 
 	router := gin.Default()
+
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://localhost:3000"}
-	config.AllowMethods = []string{"GET", "POST"}
+	config.AllowOrigins = []string{"http://localhost:3000"} // Разрешаем фронтенд
+	config.AllowMethods = []string{"GET", "POST", "OPTIONS"}
 	config.AllowHeaders = []string{"Origin", "Content-Type"}
 	router.Use(cors.New(config))
 
@@ -22,7 +39,8 @@ func main() {
 	})
 
 	router.POST("/api/rooms", func(c *gin.Context) {
-
+		roomId := generateRoomID(6)
+		c.JSON(http.StatusOK, gin.H{"roomId": roomId})
 	})
 
 	router.Run(":8080")
